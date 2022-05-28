519ed9a73a3e7a00d680f547e4858872f34ce6d2
anotherempty <anotherempty@outlook.com>
2022-05-28 12:28:28 +0300

    created square grid file

---
+++ src/routes/lib/SquareGrid.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	interface Breakpoint {
+		colCount: number;
+		gap: number;
+		width: number;
+	}
+
+	interface Span {
+		direction: 'row' | 'col';
+		amount: number;
+	}
+
+	export let colCount = 4;
+	export let gap = 0;
+	export let itemCount = 0;
+	export let breakpoints: Array<Breakpoint> = [
+		{
+			colCount: 2,
+			gap: 0,
+			width: 600
+		},
+		{
+			colCount: 3,
+			gap: 0,
+			width: 768
+		},
+		{
+			colCount: 5,
+			gap: 10,
+			width: 992
+		},
+		{
+			colCount: 6,
+			gap: 10,
+			width: 1200
+		},
+		{
+			colCount: 8,
+			gap: 10,
+			width: Infinity
+		}
+	];
+
+	let winWidth = 0;
+	let gridWidth = 0;
+	let itemSize = 0;
+
+	const span = (node: HTMLElement, { direction, amount }: Span) => {
+		let spanning = { row: 'grid-row-end', col: 'grid-column-end' }[direction];
+		Object.assign(node.style, {
+			[spanning]: `span ${amount}`
+		});
+	};
+
+	const checkBreakpoint = () => {
+		if (winWidth <= breakpoints[0].width) {
+			colCount = breakpoints[0].colCount;
+			gap = breakpoints[0].gap;
+			return;
+		}
+		for (let i = 1; i < breakpoints.length; i++) {
+			if (winWidth > breakpoints[i - 1].width && winWidth <= breakpoints[i].width) {
+				colCount = breakpoints[i].colCount;
+				gap = breakpoints[i].gap;
+				return;
+			}
+		}
+	};
+
+	onMount(() => {
+		checkBreakpoint();
+	});
+
+	$: itemSize = (gridWidth - gap * (colCount - 1)) / colCount;
+</script>
+
+<svelte:window on:resize={checkBreakpoint} bind:innerWidth={winWidth} />
+
+<div
+	class="grid"
+	style="--grid-cols:{colCount}; --grid-gap:{gap}px; --item-height:{itemSize}px"
+	bind:clientWidth={gridWidth}
+>
+	{#each { length: itemCount } as item, i}
+		<slot name="item" {span} index={i} {itemSize} />
+	{/each}
+</div>
+
+<style>
+	.grid {
+		--grid-cols: 2;
+		display: grid;
+		grid-template-columns: repeat(var(--grid-cols), 1fr);
+		gap: var(--grid-gap);
+		grid-auto-rows: var(--item-height);
+		grid-auto-flow: dense;
+	}
+</style>
