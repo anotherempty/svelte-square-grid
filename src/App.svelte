4b32efc06fd3b4c096d535c8f057f3a2ec629b5c
anotherempty <anotherempty@outlook.com>
2022-05-28 20:19:42 +0300

    created skeleton project

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Seo from "./lib/Seo.svelte";
+</script>
+
+<Seo />
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/lib/Seo.svelte
@@ -0,0 +1,27 @@
+<script>
+  import SvelteSeo from "svelte-seo";
+
+  let title = "Svelte Square Grid";
+  let description =
+    "A simple square based grid for Svelte. Can help you to easily create an image gallery like Artstation's, Windows 8 start menu, Android's photo gallery and more.";
+</script>
+
+<SvelteSeo
+  {title}
+  {description}
+  keywords="svelte, svelte-component, square, grid, image gallery"
+  openGraph={{
+    title: title,
+    description: description,
+    url: "https://anotherempty.github.io/svelte-square-grid/",
+    type: "website",
+    images: [
+      {
+        url: "https://www.example.com/images/og-image.jpg",
+        width: 850,
+        height: 650,
+        alt: "Example",
+      },
+    ],
+  }}
+/>
