81a8180166e82dca55f412b469fcd642f558365d
anotherempty <anotherempty@outlook.com>
2022-05-28 16:40:44 +0300

    added usage case

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,38 @@
+<script context="module">
+	// @ts-ignore
+	export async function load({ fetch }) {
+		const res = await fetch(`https://picsum.photos/v2/list?page=2&limit=5`);
+
+		if (res.ok) {
+			return {
+				status: res.status,
+				props: {
+					images: (await res.json()).map((img) => ({
+						src: img.download_url,
+						width: img.width,
+						height: img.height
+					}))
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load`)
+		};
+	}
+</script>
+
 <script>
 	import SquareGrid from '$lib/SquareGrid.svelte';
 	import { span } from '$lib/span';
+
+	export let images;
 </script>
 
-<SquareGrid itemCount={50}>
+<a href="/test">test</a>
+
+<SquareGrid itemCount={images.length}>
 	<div
 		slot="item"
 		let:index
@@ -12,7 +41,7 @@
 		let:itemSize
 		style="border:1px solid red;"
 	>
-		{index} : {itemSize}
+		<img src={images[index].src} alt={images[index].src} style="width:100%; height:100%; object-fit:cover;">
 	</div>
 </SquareGrid>
 
